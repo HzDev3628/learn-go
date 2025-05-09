e24a4c72536ba5fc42c4223362a55c0eccc64fa7
Artem Kharchuk <artem.khar5uk@gmail.com>
2025-05-09 21:19:22 +0300

    1: variables

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Printf("Hello Go")
+	// If we don't use "var" to create variables, we can't declare a type for the variable.
+	// var userHeight float64 = 1.8
+	// var userKg float64 = 100
+
+	// userHeight, userKg := 1.8, 100
+	userHeight := 180
+	userKg := 100
+
+	const IMTPower float64 = 2 // We can't reassign the value because it is a constant.
+
+	IMT := float64(userKg) / math.Pow(float64(userHeight) / 100, IMTPower)
+
+	fmt.Print(IMT)
 }
